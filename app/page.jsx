ea3846f3b5fac7b7bf2d0645a70477c8a61734e2
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-04-21 04:05:53 +0500

    home page secret logo click to the lounge

--- app/page.jsx
+++ app/page.jsx
@@ -22,15 +22,28 @@ export default function Home() {
         {/* Left Column */}
         <div className="flex flex-1 w-1.0 h-1.0">
           <div className="relative flex flex-1 flex-col justify-center aspect-square">
-            <div className="spline-container spline-container-left w-[150%] h-[150%]" dangerouslySetInnerHTML={{ __html: '<spline-viewer url="https://prod.spline.design/8Kk8n4FoXe4tjJBu/scene.splinecode"></spline-viewer>' }}></div>
+            <div
+              className="spline-container spline-container-left w-[150%] h-[150%]"
+              dangerouslySetInnerHTML={{
+                __html:
+                  '<spline-viewer url="https://prod.spline.design/8Kk8n4FoXe4tjJBu/scene.splinecode"></spline-viewer>',
+              }}
+            ></div>
             <div className="relative flex flex-1 flex-col justify-center aspect-square">
               <Link href="/creatives" className="cursor-pointer">
-                <h1 className="text-5xl md:text-6xl font-alta tracking-wide">CREATIVES</h1>
+                <h1 className="text-5xl md:text-6xl font-alta tracking-wide">
+                  CREATIVES
+                </h1>
               </Link>
               <div className="border-b border-white my-4"></div>
-              <Link href="/creatives-signin" className="inline-flex items-center justify-start text-white gap-4 cursor-pointer">
+              <Link
+                href="/creatives-signin"
+                className="inline-flex items-center justify-start text-white gap-4 cursor-pointer"
+              >
                 <LiaSignInAltSolid className="text-[36px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]" />
-                <span className="text-[19px] xl:text-[21px] 2xl:text-[24px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]">sign in</span>
+                <span className="text-[19px] xl:text-[21px] 2xl:text-[24px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]">
+                  sign in
+                </span>
               </Link>
             </div>
           </div>
@@ -38,20 +51,39 @@ export default function Home() {
 
         {/* Center Logo */}
         <div className="relative w-90 h-90 shrink-0">
-          <img src="/aac-logo-round.png" alt="Center Logo" className="w-full h-full object-contain hover:rotate-45 transition-transform duration-3000" />
+          <Link href={"/thelounge"}>
+            <img
+              src="/aac-logo-round.png"
+              alt="Center Logo"
+              className="w-full h-full object-contain hover:rotate-45 transition-transform duration-3000"
+            />
+          </Link>
           {/* <div className="absolute w-full h-full left-0 top-0 box-border rounded-full bg-black">Todo: Animate</div> */}
         </div>
 
         {/* Right Column */}
         <div className="relative flex flex-1 flex-col justify-center aspect-square text-right">
-          <div className="spline-container spline-container-right w-[150%] h-[150%]" dangerouslySetInnerHTML={{ __html: '<spline-viewer url="https://prod.spline.design/8Kk8n4FoXe4tjJBu/scene.splinecode"></spline-viewer>' }}></div>
+          <div
+            className="spline-container spline-container-right w-[150%] h-[150%]"
+            dangerouslySetInnerHTML={{
+              __html:
+                '<spline-viewer url="https://prod.spline.design/8Kk8n4FoXe4tjJBu/scene.splinecode"></spline-viewer>',
+            }}
+          ></div>
           <div className="relative flex flex-1 flex-col justify-center aspect-square">
             <Link href="/agencies" className="cursor-pointer">
-              <h1 className="text-5xl md:text-6xl font-alta tracking-wide">AGENCIES</h1>
+              <h1 className="text-5xl md:text-6xl font-alta tracking-wide">
+                AGENCIES
+              </h1>
             </Link>
             <div className="border-b border-white my-4"></div>
-            <Link href="/agencies-signin" className="inline-flex items-center justify-end text-white gap-4 cursor-pointer">
-              <span className="text-[19px] xl:text-[21px] 2xl:text-[24px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]">sign in</span>
+            <Link
+              href="/agencies-signin"
+              className="inline-flex items-center justify-end text-white gap-4 cursor-pointer"
+            >
+              <span className="text-[19px] xl:text-[21px] 2xl:text-[24px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]">
+                sign in
+              </span>
               <LiaSignInAltSolid className="text-[36px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]" />
             </Link>
           </div>
@@ -60,8 +92,18 @@ export default function Home() {
 
       {/* Bottom Nav */}
       <div className="absolute z-11 bottom-6 left-0 right-0 px-6 flex justify-between items-end font-semibold">
-        <Link href="/jobs-directory" className="text-[19px] xl:text-[21px] 2xl:text-[29px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]">search jobs</Link>
-        <Link href="/creatives-directory" className="text-[19px] xl:text-[21px] 2xl:text-[29px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]">hire talent</Link>
+        <Link
+          href="/jobs-directory"
+          className="text-[19px] xl:text-[21px] 2xl:text-[29px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]"
+        >
+          search jobs
+        </Link>
+        <Link
+          href="/creatives-directory"
+          className="text-[19px] xl:text-[21px] 2xl:text-[29px] transition delay-150 duration-300 ease-in-out text-white hover:text-[#ffcd1a]"
+        >
+          hire talent
+        </Link>
       </div>
     </div>
   );
