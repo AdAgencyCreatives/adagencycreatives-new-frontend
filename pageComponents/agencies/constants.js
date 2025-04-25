a947550a1e434c5a138a2001196a49babb0918e6
Asif Shabir <rao311883@gmail.com>
2025-04-25 23:24:03 +0500

    profile pages

--- pageComponents/agencies/constants.js
+++ pageComponents/agencies/constants.js
@@ -15,9 +15,35 @@ export const topMenu = [
   { label: "view pro®file", href: "#" }
 ];
 
+export const profileMenu = [
+  { label: "message ME", href: "/creatives" },
+  { label: "add me", href: "/creatives-profile" },
+  { label: "CREATE YOUR pro®file", href: "#" }
+];
+
+export const infoMenu = [
+  { label: "video", href: "/creatives" },
+  { label: "photo", href: "/creatives-profile" },
+  { label: "current work", href: "#" },
+  { label: "dashboard", href: "#" },
+  { label: "education", href: "#" }
+];
+
 export const user = {
   image: '/jobs/job3.avif',
   name: 'AD CORP.',
+  title: 'Junior Motion Graphics Designer',
+  about: '<p>I\'m an Associate Creative Director based in New York City; with a focus on art direction, brand storytelling, and concepting big ideas. I’ve been fortunate to work with brands like Keds, Louis XIII, Marantz, Wrangler, J.Crew, and USTA, crafting campaigns that blend creativity with strategy.</p><br /><p>I thrive both independently and as part of a great team. Having worked with many freelancers in my previous full-time role, I know how to adapt quickly, collaborate seamlessly, and bring ideas to life with different personalities and perspectives.</p><p>At my core, I’m a concept-first creative. There’s nothing better than zoning out with a good podcast or music while letting ideas take shape. I also love design—my natural style leans textural and street, but working in the fashion world has taught me how to adapt to different brand aesthetics while still weaving in my own perspective.</p>',
+  info: '<p>I\'m an Associate Creative Director based in New York City; with a focus on art direction, brand storytelling, and concepting big ideas. I’ve been fortunate to work with brands like Keds, Louis XIII, Marantz, Wrangler, J.Crew, and USTA, crafting campaigns that blend creativity with strategy.</p><br /><p>I thrive both independently and as part of a great team. Having worked with many freelancers in my previous full-time role, I know how to adapt quickly, collaborate seamlessly, and bring ideas to life with different personalities and perspectives.</p><p>At my core, I’m a concept-first creative. There’s nothing better than zoning out with a good podcast or music while letting ideas take shape. I also love design—my natural style leans textural and street, but working in the fashion world has taught me how to adapt to different brand aesthetics while still weaving in my own perspective.</p>',
+  workplace: 'Hybrid',
+  industry: 'Digital, Social, Technology Online',
+  media: 'Digital Interactive, Social, UI User Interface Design, UX User Experience Design, Website',
+  companysize: '1500-2000',
+  social: [
+    { label: 'linkedin', url: '#' },
+    { label: 'twitter', url: '#' },
+    { label: 'instagram', url: '#' }
+  ],
   location: 'NEW YORK, NY',
   status: 'pro member',
   plan: 'PAY ONCE',
---
+++ pageComponents/agencies/profile.jsx
@@ -0,0 +1,104 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { profileMenu, infoMenu, user } from './constants';
+import SocialItem from 'components/SocialItem';
+
+const AgenciesProfile = () => {
+  return (
+    <section className="max-w-[1600px] mx-auto px-10 pt-40">
+      <div className="grid grid-cols-3 gap-14">
+        <div className="rounded-4xl border-(--ad-gray) border shadow-(--ad-box-shadow) px-10 py-10 text-center col-span-2">
+          <div className="flex gap-10 items-start">
+            <div className="relative flex flex-col justify-between items-center gap-16 w-3xs shrink-0">
+              <div className="relative flex items-center">
+                <Image src={user.image} width="256" height="162" alt={user.title} className="image-mask" />
+              </div>
+              <div className="w-[67px] h-[67px] absolute top-0 right-0 left-0 bottom-0 flex justify-center mx-auto my-auto">
+                  <Image src="/aac-logo.png" width="67" height="67" alt={user.title} className="hover:rotate-45 transition-transform duration-3000" />
+              </div>
+              <div>
+                <div className="font-alta font-bold text-[30px] text-yellow-400">{user.name}</div>
+                <div className="text-white font-semibold text-[20px] hover:text-yellow-400 lowercase leading-[26px]">{user.title}</div>
+                <div className="my-3 border-b-2 w-[60%] mx-auto" />
+                <div className="font-alta font-bold text-[16px] text-yellow-400">{user.location}</div>
+              </div>
+            </div>
+            <div className="text-left">
+              <ul className="flex justify-between mb-10">
+                {profileMenu.map((item) => {
+                  return (
+                    <li className="lowercase text-yellow-400 hover:text-white">
+                      <Link href={item.href}>{item.label}</Link>
+                    </li>
+                  )
+                })}
+              </ul>
+              <p className="font-alta text-[16px] text-yellow-400 font-bold mb-2">ABOUT</p>
+              <p className="font-wix text-[18px]" dangerouslySetInnerHTML={{ __html: user.about }}></p>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-4xl border-(--ad-gray) border shadow-(--ad-box-shadow) px-10 py-10">
+          <div className="flex flex-col gap-14">
+            <div>
+              <p className="font-alta font-bold uppercase text-base text-yellow-400 mb-2">Workplace Preference</p>
+              <p className="lowercase hover:text-yellow-400 text-white text-xl">{user.workplace}</p>
+            </div>
+            <div>
+              <p className="font-alta font-bold uppercase text-base text-yellow-400 mb-2">Industry Specialty</p>
+              <p className="lowercase hover:text-yellow-400 text-white text-xl">{user.industry}</p>
+            </div>
+            <div>
+              <p className="font-alta font-bold uppercase text-base text-yellow-400 mb-2">Media Specialty</p>
+              <p className="lowercase hover:text-yellow-400 text-white text-xl">{user.media}</p>
+            </div>
+            <div>
+              <p className="font-alta font-bold uppercase text-base text-yellow-400 mb-2">Company Size</p>
+              <p className="lowercase hover:text-yellow-400 text-white text-xl">{user.companysize}</p>
+            </div>
+            <div>
+              <p className="font-alta font-bold uppercase text-base text-yellow-400 mb-2">SOCIAL</p>
+              <div className="flex gap-10">
+                <SocialItem item={{ label: 'linkedin', href: '#' }} />
+                <SocialItem item={{ label: 'twitter', href: '#' }} />
+                <SocialItem item={{ label: 'instagram', href: '#' }} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-4xl border-(--ad-gray) border shadow-(--ad-box-shadow) px-10 py-10 mt-40 mb-24">
+        <div className="flex gap-12">
+          <div className="flex-[45%] relative rounded-4xl overflow-hidden mt-20 shadow-(--ad-box-shadow)">
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="absolute inset-0 w-full h-full object-cover"
+            >
+              <source src="/videos/info.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="text-left flex-[55%]">
+            <ul className="flex justify-between mb-20">
+              {infoMenu.map((item) => {
+                return (
+                  <li className="lowercase text-yellow-400 hover:text-white">
+                    <Link href={item.href}>{item.label}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+            <p className="font-alta text-[16px] text-yellow-400 font-bold mb-2">Info</p>
+            <p className="font-wix text-[18px]" dangerouslySetInnerHTML={{ __html: user.info }}></p>
+            <Link href="/" className="border-yellow-400 border-4 uppercase text-yellow-400 rounded-full text-base px-20 py-4 cursor-pointer hover:border-white hover:bg-yellow-400 hover:text-white mt-10 mb-20 inline-block">CTA</Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AgenciesProfile;
