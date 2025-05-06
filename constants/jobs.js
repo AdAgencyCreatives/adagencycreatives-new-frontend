7db7d60e80e023c2b99ba1da89f8c1c2ed323c15
Asif Shabir <rao311883@gmail.com>
2025-05-07 01:01:56 +0500

    jobs directory ui

--- constants/jobs.js
+++ constants/jobs.js
@@ -8,3 +8,24 @@ export const featuredJobs = [
   { title: 'coming soon', image: '/jobs/job7.avif', agency: 'AGENCY', location: 'Dallas, TX' },
   { title: 'coming soon', image: '/jobs/job8.avif', agency: 'AGENCY', location: 'Denver, CO' },
 ];
+
+export const jobsDirectory = [
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Las Vegas, NV' },
+  { title: 'coming soon', image: '/jobs/job2.avif', agency: 'AGENCY', location: 'Brooklyn, NY' },
+  { title: 'coming soon', image: '/jobs/job3.avif', agency: 'AGENCY', location: 'Houston, TX' },
+  { title: 'coming soon', image: '/jobs/job4.avif', agency: 'AGENCY', location: 'Houston, TX' },
+  { title: 'coming soon', image: '/jobs/job5.avif', agency: 'AGENCY', location: 'Orlando, FL' },
+  { title: 'coming soon', image: '/jobs/job6.avif', agency: 'AGENCY', location: 'Phoenix, AZ' },
+  { title: 'coming soon', image: '/jobs/job7.avif', agency: 'AGENCY', location: 'Denver, CO' },
+  { title: 'coming soon', image: '/jobs/job8.avif', agency: 'AGENCY', location: 'San Francisco, CA' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Dallas, TX' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Philadelphia, PA' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Dallas, TX' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Seattle, WA' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Orlando, FL' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Orlando, FL' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'New Orleans, LA' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'New Orleans, LA' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Denver, CO' },
+  { title: 'coming soon', image: '/jobs/job1.avif', agency: 'AGENCY', location: 'Orlando, FL' },
+];
--- pageComponents/jobs/directory.jsx
+++ pageComponents/jobs/directory.jsx
@@ -1,7 +1,7 @@
 import PageHeader from "components/PageHeader";
 import JobLoopItem from "./loop/item";
 import Link from "next/link";
-import { featuredJobs } from "constants/jobs";
+import { jobsDirectory } from "constants/jobs";
 
 const JobsDirectory = () => {
   return (
@@ -13,7 +13,7 @@ const JobsDirectory = () => {
             heading="Directory"
           />
 
-          <div className="w-full sm:w-[75%] 2xl:w-[90%] sm:ms-16 lg:ms-46 xl:ms-56 2xl:ms-14 3xl:ms-20 4xl:ms-32 sm:mx-auto me-0 sm:me-auto mb-40 z-2 relative text-left">
+          <div className="w-full sm:w-[75%] 2xl:w-[90%] sm:ms-16 lg:ms-46 xl:ms-56 2xl:ms-14 3xl:ms-20 4xl:ms-32 sm:mx-auto me-0 sm:me-auto mb-0 z-2 relative text-left">
             <label className="font-wix text-xs md:text-base 2xl:text-[22px] 3xl:text-3xl 4xl:text-[40px] text-[#c2c2c2]">Search</label>
             <div className="flex items-center justify-center">
               <input
@@ -26,11 +26,11 @@ const JobsDirectory = () => {
         </div>
       </section>
       {/* Featured Jobs */}
-      <section className="p-6 md:my-20 2xl:my-16 3xl:my-50 4xl:my-80 md:px-10 mx-auto relative z-1">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-          {featuredJobs.map((job, idx) => (
+      <section className="p-6 md:px-10 mx-auto relative z-1 pb-8 pb md:pb-18 2xl:pb-24 3xl:pb-35 4xl:pb-46">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-25 2xl:pt-14 3xl:pt-40 4xl:pt-80">
+          {jobsDirectory.map((job, idx) => (
             <>
-              {idx === 6 && (
+              {idx === 16 && (
                 <div className="col-span-2 text-center flex flex-col justify-around gap-10 max-md:py-10">
                   <h2 className="relative z-1 text-2xl md:text-[44px] md:leading-[58.5px] 3xl:text-[57.07px] 3xl:leading-[78px] 4xl:text-[76.09px] 4xl:leading-[104px]">Haven't<br />Found<br />The Perfect<br />Job?</h2>
                   <div>
@@ -39,9 +39,15 @@ const JobsDirectory = () => {
                 </div>
               )}
             
-              <JobLoopItem key={idx} job={job} />
+              <JobLoopItem key={idx} job={job} className={''}/>
             </>
           ))}
+        </div>
+      </section>
+      {/* logo */}
+      <section className="pt-31 pb-21 2xl:pb-36 2xl:pt-40 3xl:pb-44 3xl:pt-33 4xl:pb-40 4xl:pt-50">
+        <div className="flex justify-center align-center">
+          <img src="/aac-logo-white.avif"  alt={''} className='w-[37px] h-[37px] lg:w-[45px] lg:h-[45px] xl:w-[48px] xl:h-[48px] md:w-[50px] md:h-[50px] 2xl:w-[61px] 2xl:h-[61px] 3xl:w-[81px] 3xl:h-[81px] 4xl:w-[108px] 4xl:h-[108px]'/>
         </div>
       </section>
       </div>
