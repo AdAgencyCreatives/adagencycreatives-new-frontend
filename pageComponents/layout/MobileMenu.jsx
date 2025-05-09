e728d70d9955b5e82d6d0cadad1b22786bfd842a
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-05-09 15:57:39 +0500

    updated mobile menu icons

--- pageComponents/layout/MobileMenu.jsx
+++ pageComponents/layout/MobileMenu.jsx
@@ -75,10 +75,10 @@ const MobileMenu = ({ isHomePage }) => {
               />
             </Link>
             <div className="flex flex-row gap-[32px] 3xl:gap-[42.67px] 4xl:gap-[56.89px]">
-              <button>
+              <button className="cursor-pointer transition delay-150 duration-300 ease-in-out text-white hover:text-brand-yellow">
                 <ChatIcon />
               </button>
-              <button>
+              <button className="cursor-pointer transition delay-150 duration-300 ease-in-out text-white hover:text-brand-yellow">
                 <CloseIcon onClick={toggleSidebar} />
               </button>
             </div>
