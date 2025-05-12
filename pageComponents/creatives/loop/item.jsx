183fdd762a8162f5c19e42431a43e4ff1ae4fd9f
Asif Shabir <rao311883@gmail.com>
2025-05-13 03:02:54 +0500

    creatives page

--- pageComponents/creatives/loop/item.jsx
+++ pageComponents/creatives/loop/item.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
  */
 const CreativeLoopItem = ({ creative, className }) => {
   return (
-    <div className={`relative py-5 mb:py-[15.75px] 2xl:py-[33.75px] 3xl:py-[44.74px] 4xl:py-[60px] rounded-3xl md:rounded-4xl border-white border-2 shadow-(--ad-box-shadow) text-center flex flex-col justify-between items-center gap-2 4xl:gap-4 ${className}`}>
+    <div className={` card relative py-5 mb:py-[15.75px] 2xl:py-[33.75px] 3xl:py-[44.74px] 4xl:py-[60px] rounded-3xl md:rounded-4xl border-white border-2 shadow-(--ad-box-shadow) text-center flex flex-col justify-between items-center gap-4 2xl:gap-8 3xl:gap-8 4xl:gap-10 ${className}`}>
       {/* <div className="relative w-[124px] h-[124px] md:w-[256px] md:h-[256px] 4xl:w-[416px] flex items-center px-4">
         <Image src={creative.image} width="256" height="162" alt={creative.title} className="image-mask mx-auto" />
       </div>
@@ -19,11 +19,11 @@ const CreativeLoopItem = ({ creative, className }) => {
             <img src="/aac-logo-white.avif"  alt={creative.title} className='w-[37px] h-[37px] lg:w-[45px] lg:h-[45px] xl:w-[48px] xl:h-[48px] md:w-[50px] md:h-[50px] 2xl:w-[61px] 2xl:h-[61px] 3xl:w-[81px] 3xl:h-[81px] 4xl:w-[108px] 4xl:h-[108px]'/>
         </div>
       </div>
-      <div className='mt-2.5 2xl:mt-4 3xl:mt-5.5 4xl:mt-7 px-[9.86px] md:px-[12.38px] 2xl:px-[16.38px] 3xl:px-[21.71px] 4xl:px-[29.22px] w-full'>
-        <div className="font-alta font-normal text-base sm:text-[16px] md:text-[20px] 2xl:text-[28px] 3xl:text-[38px] 4xl:text-[42.67px] hover:text-brand-yellow">{creative.agency}</div>
-        <div className="text-white font-inter leading-[16px] md:leading-[20px] 2xl:leading-[24px] 3xl:leading-[25px] 4xl:leading-[34.67px] font-bold text-[10px] md:text-[14px] 2xl:text-[16px] 3xl:text-[20px] 4xl:text-[25.21px] hover:text-brand-yellow lowercase">{creative.title}</div>
+      <div className='mt-2.5 2xl:mt-4 3xl:mt-6 4xl:mt-[2.071rem] px-[9.86px] md:px-[12.38px] 2xl:px-[16.38px] 3xl:px-[21.71px] 4xl:px-[29.22px] w-full'>
+        <div className="font-inter font-bold text-base hover:text-brand-yellow leading-[100%] name uppercase">{creative.agency}</div>
+        <div className="text-white font-inter font-bold hover:text-brand-yellow lowercase title">{creative.title}</div>
         <div className="my-[7.63px] md:my-2 2xl:my-[12px] 3xl:my-[15.67px] 4xl:my-[22.34px] border-b-[0.91px] md:border-b-[1.15px] 2xl:border-b-[1.5px] 3xl:border-b-[1.99px] 4xl:border-b-[2.67px] w-[100%] mx-auto" />
-        <div className="font-alta leading-[14px] md:leading-[16px] 2xl:leading-[20px] 3xl:leading-[28px] 4xl:leading-[36.4px] text-[10px] md:text-[12px] 2xl:text-[14px] 3xl:text-[18px] 4xl:text-[22.97px] hover:text-brand-yellow">{creative.location}</div>
+        <div className="font-inter hover:text-brand-yellow location">{creative.location}</div>
       </div>
     </div>
   );
