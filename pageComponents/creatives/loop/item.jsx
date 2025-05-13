fbf4f7441dd843521e8cf27165c03f29545808bd
Asif Shabir <rao311883@gmail.com>
2025-05-13 13:20:11 +0500

    creatives page fix

--- pageComponents/creatives/loop/item.jsx
+++ pageComponents/creatives/loop/item.jsx
@@ -14,7 +14,7 @@ const CreativeLoopItem = ({ creative, className }) => {
           <Image src="/aac-logo-white.avif" width="67" height="67" alt={creative.title} className="hover:rotate-45 transition-transform duration-3000" />
       </div> */}
       <div className='relative flex items-center justify-center py-3 h-[142px] md:h-[200px] 2xl:h-[224px] 3xl:h-[310px] 4xl:h-[416px]'>
-        <img src={creative.image} alt="" className='image-mask2 w-[143px] md:w-[190px] lg:w-[200px] 2xl:w-[204px] 3xl:w-[270px] 4xl:w-[362px] h-auto' />
+        <img src={creative.image} alt="" className='image-mask w-[143px] md:w-[190px] lg:w-[200px] 2xl:w-[204px] 3xl:w-[270px] 4xl:w-[362px] h-auto' />
         <div className="absolute top-[120px] md:top-[170px] lg:top-[155px] xl:top-[175px] 2xl:top-[190px] 3xl:top-[255px] 4xl:top-[328px] flex justify-center">
             <img src="/aac-logo-white.avif"  alt={creative.title} className='w-[37px] h-[37px] lg:w-[45px] lg:h-[45px] xl:w-[48px] xl:h-[48px] md:w-[50px] md:h-[50px] 2xl:w-[61px] 2xl:h-[61px] 3xl:w-[81px] 3xl:h-[81px] 4xl:w-[108px] 4xl:h-[108px]'/>
         </div>
