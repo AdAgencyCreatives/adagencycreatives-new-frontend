f5076f4b9abc090c35b2e715c7d32276067f4ef2
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-06-12 02:14:21 +0500

    fix

--- pageComponents/featured_agencies/loop/item.jsx
+++ pageComponents/featured_agencies/loop/item.jsx
@@ -13,9 +13,9 @@ const FeaturedAgenciesLoopItem = ({ agency, className='' }) => {
 
   return (
     <div className={`z-1 relative text-center flex flex-col justify-between items-center bg-black outline max-sm:outline-[0.028rem] outline-[0.075rem] md:outline-[0.092rem] xl:outline-[0.1rem] 2xl:outline-[0.106rem] 3xl:outline-[0.141rem] 4xl:outline-[0.188rem] max-sm:mx-[1.373rem] mx-[3.75rem] md:mx-[4.574rem] xl:mx-[5.002rem] 2xl:mx-[5.273rem] 3xl:mx-[7.031rem] 4xl:mx-[9.375rem] max-sm:rounded-[0.513rem] rounded-[1.4rem] md:rounded-[1.708rem] xl:rounded-[1.868rem] 2xl:rounded-[1.969rem] 3xl:rounded-[2.625rem] 4xl:rounded-[3.5rem] featured-agency-box-shadow ${className}`}>
-      <div className='relative flex items-center justify-center w-[142px] md:w-[200px] 2xl:w-[224px] 3xl:w-[310px] 4xl:w-[416px] aspect-square'>
-        <div className="image-mask">
-          <img src={agency.image} alt="" className='w-[143px] md:w-[190px] lg:w-[200px] 2xl:w-[204px] 3xl:w-[270px] 4xl:w-[362px] h-auto object-cover aspect-square' />
+      <div className='relative flex items-center justify-center max-sm:w-[3.809rem] w-[10.4rem] md:w-[12.685rem] xl:w-[13.873rem] 2xl:w-[14.625rem] 3xl:w-[19.5rem] 4xl:w-[26rem] aspect-square'>
+        <div className="">
+          <img src={agency.image} alt="" className='max-sm:w-[3.809rem] w-[10.4rem] md:w-[12.685rem] xl:w-[13.873rem] 2xl:w-[14.625rem] 3xl:w-[19.5rem] 4xl:w-[26rem] h-auto object-cover aspect-square max-sm:rounded-[0.513rem] rounded-[1.4rem] md:rounded-[1.708rem] xl:rounded-[1.868rem] 2xl:rounded-[1.969rem] 3xl:rounded-[2.625rem] 4xl:rounded-[3.5rem]' />
         </div>
       </div>
       {/* <div className="w-full">
