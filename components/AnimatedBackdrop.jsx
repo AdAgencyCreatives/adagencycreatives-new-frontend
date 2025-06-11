fbc81a6243fc2fc075c5c2a3c2f982a73debc43e
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-06-12 03:21:56 +0500

    fixes

--- components/AnimatedBackdrop.jsx
+++ components/AnimatedBackdrop.jsx
@@ -3,8 +3,8 @@
 const AnimatedBackdrop = ({className = ''}) => {
 
     return (
-        <div className={`animated-backdrop absolute inset-0 absolute h-full overflow-hidden mx-auto max-sm:w-[60%] max-sm:py-12 z-0 ${className}`}>
-            <video autoPlay loop muted playsInline className="w-full h-full object-cover max-sm:rounded-[10%] max-sm: opacity-50">
+        <div className={`animated-backdrop absolute inset-0 absolute h-full overflow-hidden mx-auto max-sm:w-[90%] max-sm:py-12 z-0 ${className}`}>
+            <video autoPlay loop muted playsInline className="w-full h-full object-cover max-sm:rounded-[50%] max-sm:opacity-100 opacity-50">
                 <source src="/videos/resources-bg.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
