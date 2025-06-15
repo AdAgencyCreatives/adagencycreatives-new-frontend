59044022aae6c83759aedaf01a33be9d14bec715
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-06-15 09:24:01 +0500

    fix

--- pageComponents/resources/loop/item.jsx
+++ pageComponents/resources/loop/item.jsx
@@ -1,7 +1,7 @@
 import LinkWrapper from 'components/LinkWrapper';
 import Image from 'next/image';
 
-const ResourceLoopItem = ({ resource, target = '_self', showLogo = true, logoClassName = '', titleClassName = '' }) => {
+const ResourceLoopItem = ({ resource, target = '_self', showLogo = true, logoClassName = '', titleClassName = '', showDescription=false }) => {
   return (
     <LinkWrapper href={resource.href} target={target} className='group transition-all'>
       <div className="relative max-sm:rounded-[0.146rem] rounded-[0.4rem] md:rounded-[0.488rem] xl:rounded-[0.534rem] 2xl:rounded-[0.563rem] 3xl:rounded-[0.75rem] 4xl:rounded-[1rem] outline max-sm:outline-[0.192rem] outline-[0.525rem] md:outline-[0.641rem] xl:outline-[0.701rem] 2xl:outline-[0.739rem] 3xl:outline-[0.985rem] 4xl:outline-[1.313rem] outline-white group-hover:outline-brand-yellow shadow-light md:shadow-(--ad-box-shadow) transition relative bg-black pt-3 md:pt-0 ">
@@ -16,8 +16,14 @@ const ResourceLoopItem = ({ resource, target = '_self', showLogo = true, logoCla
               />
             </div>
           )}
-          <p className={`absolute bottom-0 p-2 md:p-5 leading-[1.33em] text-white lowercase font-bold font-inter max-sm:text-[0.625rem] text-[1.1rem] md:text-[1.342rem] xl:text-[1.467rem] 2xl:text-[1.547rem] 3xl:text-[2.063rem] 4xl:text-[2.75rem] max-sm:mt-[0.22rem] mt-[0.6rem] md:mt-[0.732rem] xl:mt-[0.8rem] 2xl:mt-[0.844rem] 3xl:mt-[1.125rem] 4xl:mt-[1.5rem] ${titleClassName}`}
+          <div className='absolute bottom-0 p-2 md:p-5 '>
+          <p className={`leading-[1.33em] text-white lowercase font-bold font-inter max-sm:text-[0.625rem] text-[1.1rem] md:text-[1.342rem] xl:text-[1.467rem] 2xl:text-[1.547rem] 3xl:text-[2.063rem] 4xl:text-[2.75rem] max-sm:mt-[0.22rem] mt-[0.6rem] md:mt-[0.732rem] xl:mt-[0.8rem] 2xl:mt-[0.844rem] 3xl:mt-[1.125rem] 4xl:mt-[1.5rem] ${titleClassName}`}
             dangerouslySetInnerHTML={{ __html: resource.title || '' }} />
+          {showDescription && resource?.item?.description?.length > 0 && (
+            <p className={`leading-[1.33em] text-white lowercase font-bold font-inter max-sm:text-[0.22rem] text-[0.6rem] md:text-[0.732rem] xl:text-[0.8rem] 2xl:text-[0.844rem] 3xl:text-[1.125rem] 4xl:text-[1.5rem] ${titleClassName}`}
+              dangerouslySetInnerHTML={{ __html: resource.item.description || '' }} />
+          )}
+          </div>
         </div>
       </div>
     </LinkWrapper>
