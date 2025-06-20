f8ec846a8b1f39eb362f3bd2f147fb8cbf4b7749
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-06-21 02:21:07 +0500

    fix

--- components/AvatarPreloader.jsx
+++ components/AvatarPreloader.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 const AvatarPreloader = ({className = ''}) => {
     return (
          <div className={`placeholder avatar rounded-full max-sm:w-[6rem] w-[9.05rem] md:w-[11.039rem] xl:w-[12.073rem] 2xl:w-[12.727rem] 3xl:w-[16.969rem] 4xl:w-[22.625rem] max-sm:h-[6rem] h-[9.05rem] md:h-[11.039rem] xl:h-[12.073rem] 2xl:h-[12.727rem] 3xl:h-[16.969rem] 4xl:h-[22.625rem] ${className}`}></div>
--- components/ContentLinePreloader.jsx
+++ components/ContentLinePreloader.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 const ContentLinePreloader = ({className = ''}) => {
     return (
          <div className={`placeholder content-line max-sm:h-[1.2rem] h-[1.2rem] md:h-[1.464rem] xl:h-[1.601rem] 2xl:h-[1.688rem] 3xl:h-[2.25rem] 4xl:h-[3rem] max-sm:mb-[0.146rem] mb-[0.4rem] md:mb-[0.488rem] xl:mb-[0.534rem] 2xl:mb-[0.563rem] 3xl:mb-[0.75rem] 4xl:mb-[1rem] max-sm:rounded-[0.293rem] rounded-[0.8rem] md:rounded-[0.976rem] xl:rounded-[1.067rem] 2xl:rounded-[1.125rem] 3xl:rounded-[1.5rem] 4xl:rounded-[2rem] ${className}`}></div>
