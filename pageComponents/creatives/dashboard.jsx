78c1b79f41b439fb28597f7413be4e3236d0afe7
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-06-27 04:48:24 +0500

    fix

--- pageComponents/creatives/dashboard.jsx
+++ pageComponents/creatives/dashboard.jsx
@@ -42,7 +42,7 @@ const CreativesDashboard = () => {
               <MessageIcon />
               <div className="flex flex-1 flex-col max-sm:gap-[0.356rem] gap-[0.356rem] md:gap-[0.434rem] xl:gap-[0.474rem] 2xl:gap-[0.5rem] 3xl:gap-[0.667rem] 4xl:gap-[0.889rem]">
                 <LinkOrDiv href='#' className="text-white leading-[1.33em] font-semibold max-sm:text-[0.711rem] text-[0.711rem] md:text-[0.867rem] xl:text-[0.949rem] 2xl:text-[1rem] 3xl:text-[1.333rem] 4xl:text-[1.778rem]">
-                  Send a Message
+                  Send A Message
                 </LinkOrDiv>
                 <div className="text-gray-700 leading-[1.33em] max-sm:text-[0.622rem] text-[0.622rem] md:text-[0.759rem] xl:text-[0.83rem] 2xl:text-[0.875rem] 3xl:text-[1.167rem] 4xl:text-[1.556rem]">
                   continue or start a new one
