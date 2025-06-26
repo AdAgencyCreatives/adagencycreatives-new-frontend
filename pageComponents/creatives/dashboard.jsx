3373f57cbef188b23164c03e7357d25fae096244
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-06-27 04:35:25 +0500

    fix

--- pageComponents/creatives/dashboard.jsx
+++ pageComponents/creatives/dashboard.jsx
@@ -83,7 +83,7 @@ const CreativesDashboard = () => {
             <div className="flex flex-col justify-between max-sm:gap-[1.6rem] gap-[1.6rem] md:gap-[1.952rem] xl:gap-[2.134rem] 2xl:gap-[2.25rem] 3xl:gap-[3rem] 4xl:gap-[4rem]">
               <div className="flex flex-row items-center justify-between max-sm:gap-[0.711rem] gap-[0.711rem] md:gap-[0.867rem] xl:gap-[0.949rem] 2xl:gap-[1rem] 3xl:gap-[1.333rem] 4xl:gap-[1.778rem]">
                 <div className='text-white font-bold leading-none max-sm:text-[1.422rem] text-[1.422rem] md:text-[1.735rem] xl:text-[1.897rem] 2xl:text-[2rem] 3xl:text-[2.667rem] 4xl:text-[3.556rem]'>
-                  {dashboardStats?.jobs_applied > 0 ? `${dashboardStats?.jobs_applied}`.padStart(2, '0') : '0'}
+                  {dashboardStats?.jobs_applied > 0 ? dashboardStats?.jobs_applied : ''}
                 </div>
                 <BulkIcon />
               </div>
@@ -96,7 +96,7 @@ const CreativesDashboard = () => {
             <div className="flex flex-col justify-between max-sm:gap-[1.6rem] gap-[1.6rem] md:gap-[1.952rem] xl:gap-[2.134rem] 2xl:gap-[2.25rem] 3xl:gap-[3rem] 4xl:gap-[4rem]">
               <div className="flex flex-row items-center justify-between max-sm:gap-[0.711rem] gap-[0.711rem] md:gap-[0.867rem] xl:gap-[0.949rem] 2xl:gap-[1rem] 3xl:gap-[1.333rem] 4xl:gap-[1.778rem]">
                 <div className='text-white font-bold leading-none max-sm:text-[1.422rem] text-[1.422rem] md:text-[1.735rem] xl:text-[1.897rem] 2xl:text-[2rem] 3xl:text-[2.667rem] 4xl:text-[3.556rem]'>
-                  {dashboardStats?.review > 0 ? `${dashboardStats?.review}`.padStart(2, '0') : '0'}
+                  {dashboardStats?.review > 0 ? dashboardStats?.review : ''}
                 </div>
                 <MessageIcon />
               </div>
@@ -109,7 +109,7 @@ const CreativesDashboard = () => {
             <div className="flex flex-col justify-between max-sm:gap-[1.6rem] gap-[1.6rem] md:gap-[1.952rem] xl:gap-[2.134rem] 2xl:gap-[2.25rem] 3xl:gap-[3rem] 4xl:gap-[4rem]">
               <div className="flex flex-row items-center justify-between max-sm:gap-[0.711rem] gap-[0.711rem] md:gap-[0.867rem] xl:gap-[0.949rem] 2xl:gap-[1rem] 3xl:gap-[1.333rem] 4xl:gap-[1.778rem]">
                 <div className='text-white font-bold leading-none max-sm:text-[1.422rem] text-[1.422rem] md:text-[1.735rem] xl:text-[1.897rem] 2xl:text-[2rem] 3xl:text-[2.667rem] 4xl:text-[3.556rem]'>
-                  {dashboardStats?.views > 0 ? `${dashboardStats?.views}`.padStart(2, '0') : '0'}
+                  {dashboardStats?.views > 0 ? dashboardStats?.views : ''}
                 </div>
                 <DashboardUserIcon />
               </div>
