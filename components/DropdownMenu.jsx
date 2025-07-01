eb08bfacd2718af1f76b3993b69493a86811f6ec
Muzammil Ur Rehman <muzammil.rehman.upwork@gmail.com>
2025-07-02 02:54:57 +0500

    fix

--- components/DropdownMenu.jsx
+++ components/DropdownMenu.jsx
@@ -36,11 +36,12 @@ const DropdownMenu = ({ className = '', children }) => {
 
       {isOpen && (
         <div className={[
-          "dropdown-menu flex flex-col absolute top-[120%] right-[40%] bg-black outline outline-white z-999997",
-          "max-sm:p-[0.356rem] p-[0.356rem] md:p-[0.434rem] xl:p-[0.474rem] 2xl:p-[0.5rem] 3xl:p-[0.667rem] 4xl:p-[0.889rem]",
-          "max-sm:outline-[0.134rem] outline-[0.134rem] md:outline-[0.163rem] xl:outline-[0.178rem] 2xl:outline-[0.188rem] 3xl:outline-[0.251rem] 4xl:outline-[0.334rem]",
+          "dropdown-menu flex flex-col absolute top-[120%] right-[40%] bg-black outline outline-white z-999997 shadow-(--ad-box-shadow)",
+          "max-sm:py-[0.542rem] py-[0.444rem] md:py-[0.542rem] xl:py-[0.593rem] 2xl:py-[0.625rem] 3xl:py-[0.833rem] 4xl:py-[1.111rem]",
+          "max-sm:px-[0.867rem] px-[0.711rem] md:px-[0.867rem] xl:px-[0.949rem] 2xl:px-[1rem] 3xl:px-[1.333rem] 4xl:px-[1.778rem]",
+          "max-sm:outline-[0.108rem] outline-[0.089rem] md:outline-[0.108rem] xl:outline-[0.119rem] 2xl:outline-[0.125rem] 3xl:outline-[0.167rem] 4xl:outline-[0.222rem]",
           "max-sm:rounded-[0.356rem] rounded-[0.356rem] md:rounded-[0.434rem] xl:rounded-[0.474rem] 2xl:rounded-[0.5rem] 3xl:rounded-[0.667rem] 4xl:rounded-[0.889rem]",
-          "max-sm:gap-[0.356rem] gap-[0.356rem] md:gap-[0.434rem] xl:gap-[0.474rem] 2xl:gap-[0.5rem] 3xl:gap-[0.667rem] 4xl:gap-[0.889rem]",
+          "max-sm:gap-[0.542rem] gap-[0.444rem] md:gap-[0.542rem] xl:gap-[0.593rem] 2xl:gap-[0.625rem] 3xl:gap-[0.833rem] 4xl:gap-[1.111rem]",
         ].join(' ')}>
           {children}
         </div>
